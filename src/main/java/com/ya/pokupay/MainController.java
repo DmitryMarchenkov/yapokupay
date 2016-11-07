fde00af3b019e8e6d5c863ad619998e933d8c119
DmitryMarchenkov <dmitry.marchenkov92@gmail.com>
2016-11-07 19:10:20 +0200

    Crazy things 3 (before delete classes)

--- src/main/java/com/ya/pokupay/MainController.java
+++ src/main/java/com/ya/pokupay/MainController.java
@@ -1,7 +1,6 @@
 package com.ya.pokupay;
 
 import com.ya.pokupay.model.Advert;
-import com.ya.pokupay.model.Product;
 import com.ya.pokupay.service.AdvertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,8 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ya.pokupay.service.ProductService;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,14 +17,8 @@ import java.util.List;
 @RequestMapping("/")
 public class MainController {
 
-//    private ProductService productService;
     private AdvertService advertService;
 
-//    @Autowired(required=true)
-//    @Qualifier(value="productService")
-//    public void setPersonService(ProductService ps){
-//        this.productService = ps;
-//    }
 
     @Autowired(required=true)
     @Qualifier(value="advertService")

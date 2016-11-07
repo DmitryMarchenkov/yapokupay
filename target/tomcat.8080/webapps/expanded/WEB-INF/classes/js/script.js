376c3fbfcede2a73a5037313f4d2aad8bfce66ea
DmitryMarchenkov <dmitry.marchenkov92@gmail.com>
2016-11-07 19:17:26 +0200

    Resolved app

---
+++ target/tomcat.8080/webapps/expanded/WEB-INF/classes/js/script.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    var currentURL = window.location.href;
+
+    $(".dropdown-menu a").on("click", function () {
+        var url = $(this).attr("href");
+        // debugger;
+        if (url == '#') {
+            history.replaceState('', '', '/');
+        } else {
+            history.pushState('', '', url);
+        }
+
+    })
+});
--- target/tomcat.8080/webapps/expanded/resources/js/script.js
+++ target/tomcat.8080/webapps/expanded/resources/js/script.js
@@ -1,101 +1,14 @@
 $( document ).ready(function() {
     var currentURL = window.location.href;
 
-    if (/home$/.test(currentURL)) {
-        $('.pg_home').css( "left", "0px" );
-        $('.pg_home').addClass("visible");
-    }
-
-    if (/about$/.test(currentURL)) {
-        $('.pg_about').css( "left", "0px" );
-        $('.pg_about').addClass("visible");
-    }
-
-    if (/products$/.test(currentURL)) {
-        $('.pg_products').css( "left", "0px" );
-        $('.pg_products').addClass("visible");
-    }
-
-    if (/contacts$/.test(currentURL)) {
-        $('.pg_contacts').css( "left", "0px" );
-        $('.pg_contacts').addClass("visible");
-    } else {
-        history.pushState('', 'Online Shop', "/home");
-        $('.pg_home').css( "left", "0px" );
-        $('.pg_home').addClass("visible");
-    }
-
-
-    $("#menu a").on("click", function () {
-        var pgURL = window.location.href;
-        var url = $(this).attr('href');
-        if (pgURL.endsWith(url)) {
-            event.preventDefault();
-            return;
+    $(".dropdown-menu a").on("click", function () {
+        var url = $(this).attr("href");
+        // debugger;
+        if (url == '#') {
+            history.replaceState('', '', '/');
+        } else {
+            history.pushState('', '', url);
         }
 
-        var from = $(".visible");
-
-        if (/home$/.test(url)) {
-            changePage(from, $('.pg_home'));
-        }
-
-        if (/about$/.test(url)) {
-            changePage(from, $('.pg_about'));
-        }
-
-        if (/products$/.test(url)) {
-            $.ajax({
-                url:     url + '/listProducts',
-                success: function(response){
-                    $('#products').empty();
-                    $('<table id="product_table"></table>').appendTo('#products');
-                    $('<tr>').append(
-                        $('<td>').text("ID"),
-                        $('<td>').text("Name"),
-                        $('<td>').text("Price")
-                    ).appendTo('#product_table');
-
-                    $(function() {
-                        $.each(response, function(i, item) {
-                            $('<tr>').append(
-                                $('<td>').text(item.id),
-                                $('<td>').text(item.name),
-                                $('<td>').text(item.price)
-                            ).appendTo('#product_table');
-                        });
-                    });
-                    changePage(from, $('.pg_products'));
-                }
-            });
-        }
-
-        if (/contacts$/.test(url)) {
-            changePage(from, $('.pg_contacts'));
-        }
-
-        if(url != window.location){
-            window.history.pushState(null, null, url);
-        }
-        return false;
     })
-
-    $(window).bind('popstate', function() {
-        $.ajax({
-            url:     location.pathname,
-            success: function(data) {
-                $('#content').html(data);
-            }
-        });
-    });
-
 });
-
-function changePage(from, to) {
-    to.css( "left", "100%" );
-    from.animate({"left": "-=100%"}, "slow");
-    to.animate({"left": "0px"}, "slow", function () {
-        to.addClass("visible");
-        from.removeClass("visible");
-    });
-}
